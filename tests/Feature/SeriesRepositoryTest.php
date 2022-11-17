b93c57a059fb48ddf1a91337f2c1ca86e36c053d
Daniel Lima <danielrlima2015@gmail.com>
2022-11-16 22:54:22 -0300

    test SeriesRepository

---
+++ tests/Feature/SeriesRepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Requests\SeriesFormRequest;
+use App\Repositories\SeriesRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SeriesRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_when_a_series_is_created_its_seasons_and_episodes_must_also_be_created()
+    {
+        $repository = $this->app->make(SeriesRepository::class);
+        $request = new SeriesFormRequest();
+        $request->name = 'Nome da série';
+        $request->seasonsQty = 1;
+        $request->episodesPerSeason = 1;
+
+        $repository->add($request);
+
+        $this->assertDatabaseHas('series', ['name' => 'Nome da série']);
+        $this->assertDatabaseHas('seasons', ['number' => 1]);
+        $this->assertDatabaseHas('episodes', ['number' => 1]);
+    }
+}
